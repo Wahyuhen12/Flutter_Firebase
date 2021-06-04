4a401fa149bfa1c51ac6e839736624de6ab71131
Wahyu <alea08866@gmail.com>
2021-06-04 14:37:49 +0700

    update screen, widget

---
+++ lib/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/screens/home.dart';
+
+class Wrapper extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
